d65a8f09c659510648f8765898ea652b458f74df
Sam Manzano <sam.mnzn0@gmail.com>
2026-06-08 13:58:15 +0800

    chore: initialized apiService

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
     </head>
     @vite('resources/css/app.css')
@@ -12,4 +12,10 @@
         @viteReactRefresh
         @vite('resources/js/app.ts')
     </body> 
+
+    <script>
+        window.env = {
+            API_BASE_URL: '{{ env(API_BASE_URL) }}'
+        }
+    </script>
 </html>
